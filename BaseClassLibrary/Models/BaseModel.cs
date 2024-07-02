f3193df7194348b73a57aa114e6055e112d45bdd
admin <prokyonz.tech@gmail.com>
2024-07-02 11:37:45 +0530

    Added Base Model

---
+++ BaseClassLibrary/Models/BaseModel.cs
@@ -0,0 +1,28 @@
+﻿namespace BaseClassLibrary.Models
+{
+    /// <summary>
+    /// Base model for Audit Columns
+    /// </summary>
+    public class BaseModel
+    {
+        /// <summary>
+        /// CreatedDate
+        /// </summary>
+        public DateTime? CreatedDate { get; set; } = DateTime.UtcNow;
+        
+        /// <summary>
+        /// CreatedBy
+        /// </summary>
+        public string? CreatedBy { get; set; }
+
+        /// <summary>
+        /// UpdatedDate
+        /// </summary>
+        public DateTime? UpdatedDate { get; set; } = DateTime.UtcNow;
+        
+        /// <summary>
+        /// UpdatedBy
+        /// </summary>
+        public string? UpdatedBy { get; set; }
+    }
+}
